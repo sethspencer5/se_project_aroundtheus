82dddebd1c4bcd1986df110de2a49b81d3817d1a
SethSpencer5 <sethspencer550@gmail.com>
2022-12-05 12:12:32 -0700

    new update

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://github.com/sethspencer5/se_project_aroundtheus/blob/main/images/yosemite-valley.jpg?raw=true",
+      },
+    
+      {
+        name: "Lake Louise",
+        link: "https://github.com/sethspencer5/se_project_aroundtheus/blob/main/images/lake-louise.png?raw=true",
+      },
+    
+      {
+        name: "Bald Mountains",
+        link: "https://github.com/sethspencer5/se_project_aroundtheus/blob/main/images/bald-mountains.png?raw=true",
+      },
+    
+      {
+        name: "Latemar",
+        link: "https://github.com/sethspencer5/se_project_aroundtheus/blob/main/images/latemar.png?raw=true",
+      },
+    
+      {
+        name: "Vanoise National Park",
+        link: "https://github.com/sethspencer5/se_project_aroundtheus/blob/main/images/vanoise-national-park.png?raw=true",
+      },
+    
+      {
+        name: "Lago di Braies",
+        link: "https://github.com/sethspencer5/se_project_aroundtheus/blob/main/images/lago-di-braies.png?raw=true",
+      },
+]
